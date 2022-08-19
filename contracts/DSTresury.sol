0113163b7bf744cc6f88333ad4c9432ab8ab73b2
pado-peter <pado@metalabs.company>
2022-08-19 17:14:55 +0900

    로그추가

--- contracts/DSTresury.sol
+++ contracts/DSTresury.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DSTreasury is AccessControl {
     event DepositEvent(address sender, uint256 balance, string symbol); // 입금 이벤트
@@ -32,17 +34,27 @@ contract DSTreasury is AccessControl {
         tokenAddr = addr;
     }
 
-    function deposit(uint256 balance) public {
+    function deposit(uint256 balance) public payable {
+        console.log(Strings.toString(balance));
+        require(tokenAddr != address(0), "wrong address");
+
         // 컨트랙트 호출자의 토큰 잔량 체크
         uint256 userBalance = IERC20(tokenAddr).balanceOf(msg.sender);
+        console.log(Strings.toString(userBalance));
         uint256 allowance = IERC20(tokenAddr).allowance(msg.sender, address(this));
+        console.log(Strings.toString(allowance));
 
-        require(allowance >= balance && balance > 0 && userBalance >= balance, "wrong balance");
+        require(allowance >= balance && userBalance >= balance, "wrong balance");
 
         // 토큰 전송 : 컨트랙트 호출자 -> 트레저리 컨트랙트
         IERC20(tokenAddr).transferFrom(msg.sender, address(this), balance);
 
         emit DepositEvent(msg.sender, balance, _symbol);
+    }
+
+     function balance() public {
+        uint256 balance = address(this).balance;
+        console.log(Strings.toString(balance));
     }
 
     function withdraw(address targetAddr, uint256 balance) public {
