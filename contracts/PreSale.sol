52b1b7ee9523f4165df0723c4bb3ba8948ce8920
pado-peter <pado@metalabs.company>
2022-09-14 16:46:11 +0900

    구조체추가 & 함수수정

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -8,20 +8,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract PreSale is Ownable {
   event SaleEvent(address addr, uint256 balance, uint256 remainBalance);
   
-    // DST ERC20 token address
-  address public dstAddress;
-  // foundation Address
-  address public foundAddress;
-  // first preSale block number
-  uint256 public preSale1Height;
-  // second preSale block number
-  uint256 public preSale2Height;
-  // publicSale block number
-  uint256 public publicSaleHeight;
-  // sale price
-  uint256 public salePrice;
+    
 
-  // Pre-sale Account Info
+  // DST ERC20 token address
+  address dstAddress;
+  // foundation Address
+  address foundAddress;
+
+  // PreSaleInfo
+  struct PreSaleInfo {
+    // first preSale block number
+    uint256 preSale1Height;
+    // second preSale block number
+    uint256 preSale2Height;
+    // publicSale block number
+    uint256 publicSaleHeight;
+    // sale price
+    uint256 salePrice;
+  }
+
+  // Pre-sale Account
   struct PreSaleAccount {
     // total able claim balance
     uint256 totalClaimBalance;
@@ -31,7 +37,21 @@ contract PreSale is Ownable {
     bool isExist;
   }
 
+  mapping(uint8 => PreSaleInfo) public preSaleInfo;
   mapping(address => PreSaleAccount) public preSaleAccounts;
+
+  // [관리자] 프리세일 일정 정보 입력(판매가격, 1차 프리세일 시작 시점, 2차 프리세일 시작 시점, 퍼블릭 세일 시작 시점)
+  function setPresaleInfo(uint256 _salePrice, uint256 _preSale1Height, uint256 _preSale2Height, uint256 _publicSaleHeight ) public onlyOwner {
+    require(_preSale2Height > _preSale1Height, "presale2 more than presale1");
+    require(_publicSaleHeight > _preSale2Height, "publicSale more than presale2");
+    preSaleInfo[0] = PreSaleInfo({
+      salePrice: _salePrice,
+      preSale1Height: _preSale1Height,
+      preSale2Height: _preSale2Height,
+      publicSaleHeight: _publicSaleHeight
+    });
+
+  }
 
 
   // [관리자] 재단 지갑 입력
@@ -65,21 +85,6 @@ contract PreSale is Ownable {
 
 
 
-  // [관리자] 프리세일 일정 정보 입력(1차 프리세일 시작 시점, 2차 프리세일 시작 시점, 퍼블릭 세일 시작 시점)
-  function setPresaleInfo(uint256 _preSale1Height, uint256 _preSale2Height, uint256 _publicSaleHeight ) public onlyOwner {
-    require(_preSale2Height > _preSale1Height, "presale2 more than presale1");
-    require(_publicSaleHeight > _preSale2Height, "publicSale more than presale2");
-    preSale1Height = _preSale1Height;
-    preSale2Height = _preSale2Height;
-    publicSaleHeight = _publicSaleHeight;
-  }
-
-  // [관리자] DST 개당 ETH 가격 입력
-  // [관리자] 프리세일 일정 정보 입력(1차 프리세일 시작 시점, 2차 프리세일 시작 시점, 퍼블릭 세일 시작 시점)
-  function setSalePrice(uint256 _price ) public onlyOwner {
-    salePrice = _price;
-  }
-
   // [관리자] 컨트랙트 ETH 잔액 조회
   function getEthBalance( ) public view returns(uint256){
     return address(this).balance;
@@ -95,14 +100,14 @@ contract PreSale is Ownable {
 // [관리자] 재단 지갑으로 ETH 출금
   function withdrawETH( ) public onlyOwner {
     require(foundAddress != address(0), "no foundation address");
+    // Transfer Eth to Foundation address
     payable(foundAddress).transfer(address(this).balance);
   }
 
   // [관리자] 재단 지갑으로 DST 출금
   function withdrawDST( ) public onlyOwner {
     require(foundAddress != address(0), "no foundation address");
-
-    // Transfer DST: Contract -> Contract caller
+    // Transfer DST: Contract -> Foundation address
     IERC20(dstAddress).transfer(foundAddress, IERC20(dstAddress).balanceOf(address(this)));
   }
 
@@ -111,25 +116,27 @@ contract PreSale is Ownable {
 // [유저] ETH로 DST 구매
   function sale( ) public payable{
     uint price = msg.value;
+    PreSaleInfo memory saleInfo = preSaleInfo[0];
 
+    require(saleInfo.salePrice > 0, "sale price more than zero");
     require(price > 0, "price more than zero");
-    require(price % salePrice == 0, "price error");
+    require(price % saleInfo.salePrice == 0, "price error");
 
     uint blockNumber = block.number;
-    uint256 amount = price / salePrice;
-    if(blockNumber >= preSale1Height && blockNumber < preSale2Height )     {
+    uint256 amount = price / saleInfo.salePrice;
+    if(blockNumber >= saleInfo.preSale1Height && blockNumber < saleInfo.preSale2Height )     {
       // 1차 판매중
       PreSaleAccount storage account = preSaleAccounts[msg.sender];
       require(account.totalClaimBalance - account.claimedBalance > amount, "Not enough quantity available to sale");
       IERC20(dstAddress).transfer(msg.sender, amount);
       account.claimedBalance += amount;
-    } else if(blockNumber >= preSale2Height && blockNumber < publicSaleHeight )     {
+    } else if(blockNumber >= saleInfo.preSale2Height && blockNumber < saleInfo.publicSaleHeight )     {
       // 2차판매
       require(preSaleAccounts[msg.sender].isExist, "Not exist whitelist");
       uint balance = IERC20(dstAddress).balanceOf(address(this));
       require(balance > amount, "Insufficient quantity.");
       IERC20(dstAddress).transfer(msg.sender, amount);
-    } else if(blockNumber >= publicSaleHeight) {
+    } else if(blockNumber >= saleInfo.publicSaleHeight) {
       // 퍼블릭세일
       revert("not saled");
     } else {
