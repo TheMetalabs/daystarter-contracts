8c2de9a77bfb30b4022ee11a3f412fedf042234a
Ringo <ringo@metalabs.company>
2022-12-08 18:40:27 +0900

    Remove unused tests

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,23 +1,5 @@
-const DSP = artifacts.require('DSP');
-const DSPTreasury = artifacts.require('DSPTreasury');
 const DST = artifacts.require('DST');
-const DSTTreasury = artifacts.require('DSTTreasury');
-const Membership = artifacts.require('Membership');
-const MembershipTreasury = artifacts.require('MembershipTreasury');
-const Benefit = artifacts.require('Benefit');
-const BenefitTreasury = artifacts.require('BenefitTreasury');
-const Achievement = artifacts.require('Achievement');
-const Vesting = artifacts.require('Vesting');
 
 module.exports = function (deployer) {
-  deployer.deploy(DSP);
   deployer.deploy(DST);
-  deployer.deploy(DSPTreasury);
-  deployer.deploy(DSTTreasury);
-  deployer.deploy(Membership);
-  deployer.deploy(Benefit);
-  deployer.deploy(MembershipTreasury);
-  deployer.deploy(Achievement);
-  deployer.deploy(BenefitTreasury);
-  deployer.deploy(Vesting);
 };
--- test/DST.js
+++ test/DST.js
@@ -1,6 +1,8 @@
 const DST = artifacts.require('DST');
 
 contract('DST', async (accounts) => {
+  const owner = accounts[0];
+
   it('should put 1,000,000,000 DST in the first account', async () => {
     const DSTInstance = await DST.deployed();
     const balance = await DSTInstance.balanceOf.call(accounts[0]);
@@ -10,5 +12,18 @@ contract('DST', async (accounts) => {
       1000000000 * 10 ** 18,
       '1,000,000,000 wasn\'t in the first account'
     );
+  });
+
+  describe('mint', () => {
+    it('should raise an error when owner is trying to mint', async () => {
+      let error = false;
+      const DSTInstance = await DST.deployed();
+      try {
+        await DSTInstance.mint(owner, 10000000, { from: owner });
+      } catch (e) {
+        error = true;
+      }
+      assert.equal(error, true);
+    });
   });
 });
